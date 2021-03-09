0528bf9df0fa79931e3f0ad8eb810036f8e84fa4
Chris Caruso <chriscaruso@live.com>
2021-03-08 23:34:31 -0800

    move to angular projects + libs

---
+++ projects/mimic/README.md
@@ -0,0 +1,9 @@
+# Mimic
+
+``` ts
+interface Library {
+    name: 'mimic',
+    version: '0.0.1',
+    projectDescrition: string;
+}
+```
--- projects/mimic/karma.conf.js
+++ projects/mimic/karma.conf.js
@@ -25,7 +25,7 @@ module.exports = function (config) {
       suppressAll: true // removes the duplicated traces
     },
     coverageReporter: {
-      dir: require('path').join(__dirname, './coverage/eventhub'),
+      dir: require('path').join(__dirname, '../../coverage/mimic'),
       subdir: '.',
       reporters: [
         { type: 'html' },
